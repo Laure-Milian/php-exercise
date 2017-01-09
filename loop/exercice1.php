1695fb1cd442b096eee40cfe4ac1a5eb8139f2d6
Laure <laure.milian@gmail.com>
2017-01-09 10:02:59 +0100

    ajout des consignes exercice 1 pour suivi

--- loop/exercice1.php
+++ loop/exercice1.php
@@ -1,7 +1,15 @@
+<h1>Exercice 1</h1>
+<p>Créer une variable et l'initialiser à 0.
+Tant que cette variable n'atteint pas 10, il faut :
+
+l'afficher
+l'incrementer
+</p>
+
 <?php
 
 	$i = 0;
 	$len = 10;
 	for ($i=0; $i<=$len; $i++) {
-		echo $i;
+		echo '<div>' . $i . '</div>';
 	}
