4975e58cd46eae90f66d0f161b185b09421f271f
Laure <laure.milian@gmail.com>
2017-01-09 10:03:21 +0100

    exercice 2 terminé

---
+++ loop/exercice2.php
@@ -0,0 +1,15 @@
+<h1>Exercice 2</h1>
+<p>Créer deux variables. Initialiser la première à 0 et la deuxième avec un nombre compris en 1 et 100.
+Tant que la première variable n'est pas supérieur à 20 :
+
+multiplier la première variable avec la deuxième
+afficher le résultat
+incrementer la première variable</p>
+
+<?php
+$b = 53;
+$len = 20;
+
+for ($a = 0; $a <= $len; $a++) {
+	echo '<div>' . $a * $b . '</div>';
+}
