59bd40486a283c14c3f286c3f2959ddc399c2b0d
Laure <laure.milian@gmail.com>
2017-01-09 11:23:11 +0100

    exercice 2 terminé

--- fonctions/exercice2.php
+++ fonctions/exercice2.php
@@ -1,2 +1,10 @@
 <h1>Fonctions - Exercice 2</h1>
 <p>Faire une fonction qui prend en paramètre une chaine de caractères et qui retourne cette même chaine.</p>
+
+<?php
+	function StrRet($str) {
+		return $str;
+	}
+
+	echo StrRet("Bonjour le monde");
+?>
