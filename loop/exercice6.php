c752c1525f212337134cc4307275d27f38b57fb9
Laure <laure.milian@gmail.com>
2017-01-09 10:58:16 +0100

    exercice 6 terminé

--- loop/exercice6.php
+++ loop/exercice6.php
@@ -2,3 +2,9 @@
 <p>
 En allant de 20 à 0 avec un pas de 1, afficher le message C'est presque bon.
 </p>
+<?php
+	$len = 0;
+	for ($i = 20 ; $i >= $len ; $i--) {
+		echo "<div>" . $i . " C'est presque bon. </div>";
+	}
+?>
