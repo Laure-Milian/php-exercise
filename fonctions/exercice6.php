835023f72069fab230616584d0451d1c143828d0
Laure <laure.milian@gmail.com>
2017-01-09 12:10:32 +0100

    exercice 6 terminé

--- fonctions/exercice6.php
+++ fonctions/exercice6.php
@@ -1,2 +1,10 @@
 <h1>Fonctions - Exercice 6</h1>
 <p>Faire une fonction qui prend trois paramètres : nom, prenom et age. Elle doit renvoyer une chaine de la forme : "Bonjour" + nom + prenom + ",tu as" + age + "ans".</p>
+
+<?php
+	function Concat($nom, $prenom, $age) {
+		return "Bonjour " . $nom . " " . $prenom . ", tu as " . $age . " ans.";
+	}
+
+	echo Concat('Dupond', 'Frank', 27);
+?>
