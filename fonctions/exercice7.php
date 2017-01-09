1927d147ed5efedddd1476df94c24ee06132d09a
Laure <laure.milian@gmail.com>
2017-01-09 13:45:05 +0100

    exercice 7 terminé

--- fonctions/exercice7.php
+++ fonctions/exercice7.php
@@ -10,3 +10,27 @@
 	</ul>
 </p>
 <p>Gérer tous les cas.</p>
+
+<?php
+	function Describe($genre, $age) {
+
+		$genre_low = strtolower($genre);
+		if($genre_low === "homme" && $age >= 18) {
+			return 'Vous êtes un homme et vous êtes majeur';
+		}
+		elseif($genre_low === "homme" && $age < 18) {
+			return 'Vous êtes un homme et vous êtes mineur';
+		}
+		elseif($genre_low === "femme" && $age >= 18) {
+			return 'Vous êtes une femme et vous êtes majeure';
+		}
+		elseif($genre_low === "femme" && $age < 18) {
+			return 'Vous êtes une femme et vous êtes mineure';
+		}
+	}
+
+	echo '<div>' . Describe("Homme", 26) . '</div>';
+	echo '<div>' . Describe("homme", 14) . '</div>';
+	echo '<div>' . Describe("femme", 26) . '</div>';
+	echo '<div>' . Describe("femme", 14) . '</div>';
+?>
