84e0fdf656349a1b59731287e2880a4aecba078c
Laure <laure.milian@gmail.com>
2017-01-09 11:57:36 +0100

    exercice4 terminé

--- fonctions/exercice4.php
+++ fonctions/exercice4.php
@@ -6,3 +6,24 @@
 		<li>Les deux nombres sont identiques si les deux nombres sont égaux</li>
 	</ul>
 </p>
+
+<?php
+	function Compare($a, $b) {
+		if(!is_numeric($a) || !is_numeric($b)) {
+			return "Au moins l'un des paramètres n'est pas un nombre.";
+		} elseif ($a > $b) {
+			return 'Le premier nombre est plus grand.';
+		} 
+		elseif ($a < $b) {
+			return 'Le premier nombre est plus petit.';
+		} 
+		elseif ($a === $b) {
+			return 'Les deux nombres sont identiques.';
+		}
+	}
+
+	echo '<div>' . Compare(10, 5) . '</div>';
+	echo '<div>' . Compare(5, 10) . '</div>';
+	echo '<div>' . Compare(5, 5) . '</div>';
+	echo '<div>' . Compare("bla", 5) . '</div>';
+?>
