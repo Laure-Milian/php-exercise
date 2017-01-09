0cf7e9a617fbc37d97efe594677235f53e064c97
Laure <laure.milian@gmail.com>
2017-01-09 14:35:42 +0100

    exercice 10 terminé

---
+++ array/exercice10.php
@@ -0,0 +1,11 @@
+<?php
+
+$arrAssoc = [02=>"Aisne",
+			59=>"Nord",
+			60=>"Oise",
+			62=>"Pas de Calais",
+			80=>"Somme"];
+
+foreach ($arrAssoc as $number => $name) {
+	echo '<div> Le département ' . $name . ' a le numéro ' . $number . '. </div>';
+}
