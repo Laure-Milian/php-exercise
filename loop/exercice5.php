780f5632efea1022329a9499fb7d206902bc7df3
Laure <laure.milian@gmail.com>
2017-01-09 10:50:41 +0100

    exercice 5 terminé

--- loop/exercice5.php
+++ loop/exercice5.php
@@ -2,3 +2,10 @@
 <p>
 En allant de 1 à 15 avec un pas de 1, afficher le message On y arrive presque.
 </p>
+
+<?php
+	$len = 15;
+	for ($i = 1 ; $i <= $len ; $i++) {
+		echo '<div>' . $i . ' -> On y arrive presque </div>'; 
+	}
+?>
