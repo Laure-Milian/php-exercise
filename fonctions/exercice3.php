2e8bb320e80f75689044eb633381388376d6acbf
Laure <laure.milian@gmail.com>
2017-01-09 11:26:18 +0100

    exercice 3 terminé

--- fonctions/exercice3.php
+++ fonctions/exercice3.php
@@ -1,2 +1,10 @@
 <h1>Fonctions - Exercice 3</h1>
 <p>Faire une fonction qui prend en paramètre deux chaines de caractères et qui revoit la concaténation de ces deux chaines</p>
+
+<?php
+	function ConcatStr($str1, $str2) {
+		return $str1 . ' ' . $str2;
+	}
+
+	echo ConcatStr("Hola", "el mundo");
+?>
