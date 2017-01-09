cc532d635f133a4ad776051fe792522deaa45f68
Laure <laure.milian@gmail.com>
2017-01-09 16:58:27 +0100

    Form - Exercice 1 - Création d'un formulaire demandant le nom et le prénom. Ce formulaire redirige vers la page user.php avec la méthode GET.

---
+++ form/exercice1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Form</title>
+</head>
+<body>
+	<form action="/user.php" method="get">
+		<div>
+			<label for="nom">Nom :</label>
+		</div>
+		<div>
+			<input type="text" id="nom" name="nom">
+		</div>
+		<div>
+			<label for="prenom">Prénom :</label>
+		</div>
+		<div>
+			<input type="text" id="prenom" name="prenom">
+		</div>
+		<div>
+			<input type="submit" value="Envoyer">
+		</div>
+	</form>
+
+
+</body>
+</html>
