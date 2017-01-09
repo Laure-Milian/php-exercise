fa0781bbf4e837d0000707ff09e54442d7510f84
Laure <laure.milian@gmail.com>
2017-01-09 11:08:43 +0100

    exercice 8 terminé

--- loop/exercice8.php
+++ loop/exercice8.php
@@ -2,3 +2,9 @@
 <p>
 En allant de 200 à 0 avec un pas de 12, afficher le message Enfin !!!!.
 </p>
+<?php
+	$len = 0;
+	for ($i = 200; $i >= $len; $i = $i - 12) {
+		echo '<div>' . $i . ' Enfin !!!! </div>';
+	}
+?>
