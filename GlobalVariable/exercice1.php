2ecbb60696ff86ffb5facfd6c16afedb536f2c9c
Laure <laure.milian@gmail.com>
2017-01-10 10:17:18 +0100

    initial commit GlobalVariable

---
+++ GlobalVariable/exercice1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Global Variables Exercice</title>
+	<link rel="stylesheet" href="exercice1.css">
+</head>
+<body>
+
+	<?php
+		$User_Agent = $_SERVER['HTTP_USER_AGENT'];
+		$IP_Address = $_SERVER['REMOTE_ADDR'];
+		$Server_name = $_SERVER['SERVER_NAME'];
+	?>
+
+	<div class="container">
+		<div> 
+			<h2>Votre User Agent :</h2>
+			<span><?= $User_Agent ?></span>
+		</div>
+		<div> 
+			<h2>Votre addresse IP :</h2>
+			<span><?= $IP_Address ?></span>
+		</div>
+		<div> 
+			<h2>Nom du serveur :</h2>
+			<span><?= $Server_name ?></span>
+		</div>
+		
+	</div>
+	
+</body>
+</html>
