4fc0f95b46b090eecbb9556848013e148276f4c0
Laure <laure.milian@gmail.com>
2017-01-10 16:40:46 +0100

    initial commit TP

---
+++ dates/TP/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Calendrier</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="container">
+		<form action="/">
+			<div>
+				<label for="month">Sélectionner le mois : </label>
+			</div>
+			<div>
+				<select name="month" id="month">
+					<option value="01">Janvier</option>
+					<option value="02">Février</option>
+				</select>
+			</div>
+			<div>
+				<label for="year">Sélectionner l'année : </label>
+			</div>
+			<div>
+				<select name="year" id="year">
+					<option value="2016">2016</option>
+					<option value="2017">2017</option>
+				</select>
+			</div>
+		</form>
+	</div>
+	
+</body>
+</html>
